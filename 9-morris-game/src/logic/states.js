fe90c704d47016857ad079e12b6f6a6fd20a3ebd
lukvaz <lukas.vasquez.verdejo@gmail.com>
2024-07-13 14:12:28 -0400

    refactor: cleaning duplicated code in logic

--- 9-morris-game/src/logic/states.js
+++ 9-morris-game/src/logic/states.js
@@ -11,15 +11,11 @@ function checkElimination(board, turn, index) {
 function getOppositePlayer(turn) {
     return turn === DEFAULT_CONFIGURATION.PLAYER1["ID"] ? DEFAULT_CONFIGURATION.PLAYER2["ID"] : DEFAULT_CONFIGURATION.PLAYER1["ID"]
 }
-function checkWinning( context,player){
-    return context[player].onGamePieces.length <3 && context[player].playedPieces==9
-     
+function checkWinning(context, player) {
+    return context[player].onGamePieces.length < 3 && context[player].playedPieces == 9
+
 }
 
-// TODO
-// chequear movimiento sea valido
-// si no valido-> retoranr el mismo estado
-// si es valido-> chequar  si hay eliminación y actualizar estados
 class Positioning {
     isValidMove(index, board, turn) {
         return board[index] == null
@@ -27,7 +23,6 @@ class Positioning {
     update(index, context) {
         // click on an ocupped position
         if (!this.isValidMove(index, context.board, context.turn)) {
-            console.log("Positioning:invalid move")
             return context
         }
         //update context
@@ -38,7 +33,6 @@ class Positioning {
 
         //if theres any elimination  change state to Eliminating
         if (checkElimination(newContext.board, newContext.turn, index)) {
-            console.log("to eliminate status")
             let newState = new Eliminating()
             newContext[context.turn].state = newState
             return newContext
@@ -58,42 +52,45 @@ class Eliminating {
     update(index, context) {
         // click on an invalid position
         if (!this.isValidMove(index, context.board, context.turn)) {
-            console.log("Eliminating:invalid move")
             return context
         }
         // clicks on an opponent piece
         else {
-            //eliminate that piece and update state to positioning
+            //eliminate  piece and update state
             let newContext = { ...context }
-            newContext.board[index] = null
-            newContext[context.turn].state = newContext[context.turn].onGamePieces.length <= 3 && newContext[context.turn].playedPieces==9  ? new Ending():newContext[context.turn].playedPieces==9? new Playing(): new Positioning()
-            // updating oponent status
             let newTurn = getOppositePlayer(context.turn)
-            newContext[newTurn].onGamePieces = newContext[newTurn].onGamePieces.filter(i => i !== index)
+            this.eliminatePiece(index, newContext, newTurn)
             // checks for winning condition
-            if (checkWinning(newContext,newTurn)){
+            if (checkWinning(newContext, newTurn)) {
                 newContext[newTurn].state = new Loser()
                 newContext[context.turn].state = new Winner()
                 newContext.turn = null
-                newContext.winner=1
+                newContext.winner = 1
                 return newContext
             }
-            newContext[newTurn].state = newContext[newTurn].onGamePieces.length <= 3 && newContext[newTurn].playedPieces==9  ? new Ending():newContext[newTurn].playedPieces==9? new Playing(): new Positioning()
+            // updates states and turn
+            newContext[context.turn].state = this.updateState(context, context.turn)
+            newContext[newTurn].state = this.updateState(newContext, newTurn)
             newContext.turn = newTurn
             return newContext
         }
+
     }
-    updateState(context,player){
-            switch (true) {
-                case context[player].onGamePieces.length <= 3 && context[player].playedPieces == 9:
-                    return new Ending();
-                case context[player].playedPieces == 9:
-                    return new Playing();
-                default:
-                    return new Positioning();
-            }
+    updateState(context, player) {
+        switch (true) {
+            case context[player].onGamePieces.length <= 3 && context[player].playedPieces == 9:
+                return new Ending();
+            case context[player].playedPieces == 9:
+                return new Playing();
+            default:
+                return new Positioning();
         }
     }
+    eliminatePiece(index, context, player) {
+        context.board[index] = null
+        context[player].onGamePieces = context[player].onGamePieces.filter(i => i !== index)
+    }
+}
 
 class Playing {
     selected_piece = null
@@ -106,7 +103,6 @@ class Playing {
 
     update(index, context) {
         if (!this.isValidMove(index, context.board, context.turn)) {
-            console.log("Playing:invalid move")
             this.selected_piece = null
             return context
         }
@@ -116,26 +112,24 @@ class Playing {
             return context
         }
         // moving piece
-        else {
-            let newContext = { ...context }
-            // updating board and player status
-            newContext.board[this.selected_piece] = null
-            newContext.board[index] = context.turn
-            newContext[context.turn].onGamePieces = newContext[context.turn].onGamePieces.map(i => i === this.selected_piece ? index : i)
-
-            this.selected_piece = null
-            // check for elimination
-            if (checkElimination(newContext.board, newContext.turn, index)) {
-                console.log("to eliminate status")
-                let newState = new Eliminating()
-                newContext[context.turn].state = newState
-            }
-            else {
-                newContext.turn = getOppositePlayer(context.turn)
-            }
-            console.log(newContext)
+        let newContext = { ...context }
+        this.movePiece(index, newContext)
+        
+        // check for elimination
+        if (checkElimination(newContext.board, newContext.turn, index)) {
+            let newState = new Eliminating()
+            newContext[context.turn].state = newState
             return newContext
         }
+        // setting next turn 
+        newContext.turn = getOppositePlayer(context.turn)
+        return newContext
+    }
+    movePiece(index, context) {
+        context.board[this.selected_piece] = null
+        context.board[index] = context.turn
+        context[context.turn].onGamePieces = context[context.turn].onGamePieces.map(i => i === this.selected_piece ? index : i)
+        this.selected_piece = null
     }
 }
 
@@ -149,7 +143,6 @@ class Ending {
     }
     update(index, context) {
         if (!this.isValidMove(index, context.board, context.turn)) {
-            console.log("Ending:invalid move")
             this.selected_piece = null
             return context
         }
@@ -159,26 +152,24 @@ class Ending {
             return context
         }
         // moving piece
-        else {
-            let newContext = { ...context }
-            // updating board and player status
-            newContext.board[this.selected_piece] = null
-            newContext.board[index] = context.turn
-            newContext[context.turn].onGamePieces = newContext[context.turn].onGamePieces.map(i => i === this.selected_piece ? index : i)
-
-            this.selected_piece = null
-            // check for elimination
-            if (checkElimination(newContext.board, newContext.turn, index)) {
-                console.log("to eliminate status")
-                let newState = new Eliminating()
-                newContext[context.turn].state = newState
-            }
-            else {
-                newContext.turn = getOppositePlayer(context.turn)
-            }
-            console.log(newContext)
+        let newContext = { ...context }
+        this.movePiece(index, newContext)
+        // check for elimination
+        if (checkElimination(newContext.board, newContext.turn, index)) {
+            let newState = new Eliminating()
+            newContext[context.turn].state = newState
             return newContext
         }
+        // setting next turn
+        newContext.turn = getOppositePlayer(context.turn)
+        return newContext
+    }
+
+    movePiece(index, context) {
+        context.board[this.selected_piece] = null
+        context.board[index] = context.turn
+        context[context.turn].onGamePieces = context[context.turn].onGamePieces.map(i => i === this.selected_piece ? index : i)
+        this.selected_piece = null
     }
 }
 
